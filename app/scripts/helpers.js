ef03febb596d5da52581303b9fb31a5737587723
George Strakhov <george.strakhov@gmail.com>
2015-09-12 15:53:27 +0800

    something

--- app/scripts/helpers.js
+++ app/scripts/helpers.js
@@ -3,18 +3,18 @@ app.helpers = {};
 
 app.helpers.shake = function(div) {
 
-    var interval = 100;                                                                                                 
-    var distance = 10;                                                                                                  
-    var times = 4;                                                                                                      
+  var interval = 100;                                                                                                 
+  var distance = 10;                                                                                                  
+  var times = 4;                                                                                                      
 
-    $(div).css('position','relative');                                                                                  
+  $(div).css('position','relative');                                                                                  
 
-    for(var iter=0;iter<(times+1);iter++){                                                                              
-        $(div).animate({ 
-            left:((iter%2==0 ? distance : distance*-1))
-            },interval);                                   
-    }//for                                                                                                              
+  for(var iter=0;iter<(times+1);iter++){                                                                              
+    $(div).animate({ 
+      left:((iter%2==0 ? distance : distance*-1))
+    },interval);                                   
+  }//for                                                                                                              
 
-    $(div).animate({ left: 0},interval);                                                                                
+  $(div).animate({ left: 0},interval);                                                                                
 
 }//shake 
